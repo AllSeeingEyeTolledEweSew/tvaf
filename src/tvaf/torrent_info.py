20eb1b0ef107aec5bd209b0a0d113896a2a4a83c
AllSeeingEyeTolledEweSew <asetes@pm.me>
2022-09-08 07:18:15 +0000

    [concurrency] use a task group for first-value torrent info functions

--- src/tvaf/torrent_info.py
+++ src/tvaf/torrent_info.py
@@ -12,33 +12,47 @@
 # PERFORMANCE OF THIS SOFTWARE.
 from __future__ import annotations
 
-from collections.abc import Awaitable
+import asyncio
+from collections.abc import Coroutine
 from collections.abc import Iterable
+import functools
+from typing import Any
 from typing import Callable
 from typing import TypeVar
 
+import anyio
 import libtorrent as lt
 
 from tvaf import caches
 
-from . import concurrency
 from . import plugins
 
 _T = TypeVar("_T")
 
 
-async def _first_from_plugins(aws: Iterable[Awaitable[_T]]) -> _T:
-    # TODO: should we report unexpected exceptions from runners-up?
-    with concurrency.as_completed_ctx(aws) as iterator:
-        for future in iterator:
+async def _first_from_plugins(
+    funcs: Iterable[Callable[[], Coroutine[Any, Any, _T]]]
+) -> _T:
+    first_result: asyncio.Future[_T] = asyncio.get_event_loop().create_future()
+    async with anyio.create_task_group() as task_group:
+
+        async def run(func: Callable[[], Coroutine[Any, Any, _T]]):
             try:
-                return await future
+                result = await func()
             except KeyError:
-                pass
+                return
+            if not first_result.done():
+                first_result.set_result(result)
+            task_group.cancel_scope.cancel()
+
+        for func in funcs:
+            task_group.start_soon(functools.partial(run, func))
+    if first_result.done():
+        return first_result.result()
     raise KeyError()
 
 
-MapFile = Callable[[lt.info_hash_t, int], Awaitable[tuple[int, int]]]
+MapFile = Callable[[lt.info_hash_t, int], Coroutine[Any, Any, tuple[int, int]]]
 _MAP_FILE_FUNCS: plugins.Funcs[MapFile] = plugins.Funcs("tvaf.torrent_info.map_file")
 map_file_plugin = _MAP_FILE_FUNCS.decorator
 
@@ -46,10 +60,12 @@ map_file_plugin = _MAP_FILE_FUNCS.decorator
 @caches.alru_cache(maxsize=256)
 async def map_file(info_hashes: lt.info_hash_t, file_index: int) -> tuple[int, int]:
     funcs = _MAP_FILE_FUNCS.get().values()
-    return await _first_from_plugins([func(info_hashes, file_index) for func in funcs])
+    return await _first_from_plugins(
+        [functools.partial(func, info_hashes, file_index) for func in funcs]
+    )
 
 
-IsPrivate = Callable[[lt.info_hash_t], Awaitable[bool]]
+IsPrivate = Callable[[lt.info_hash_t], Coroutine[Any, Any, bool]]
 _IS_PRIVATE_FUNCS: plugins.Funcs[IsPrivate] = plugins.Funcs(
     "tvaf.torrent_info.is_private"
 )
@@ -59,4 +75,6 @@ is_private_plugin = _IS_PRIVATE_FUNCS.decorator
 @caches.alru_cache(maxsize=256)
 async def is_private(info_hashes: lt.info_hash_t) -> bool:
     funcs = _IS_PRIVATE_FUNCS.get().values()
-    return await _first_from_plugins([func(info_hashes) for func in funcs])
+    return await _first_from_plugins(
+        [functools.partial(func, info_hashes) for func in funcs]
+    )
