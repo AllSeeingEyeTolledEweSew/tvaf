d72878e48b4554cea6e3d046e14f87944b9a1f53
AllSeeingEyeTolledEweSew <asetes@pm.me>
2022-10-02 17:18:11 +0000

    [concurrency] taskify get_torrent_info()

--- src/tvaf/services/util.py
+++ src/tvaf/services/util.py
@@ -16,6 +16,7 @@ from __future__ import annotations
 import asyncio
 from typing import Optional
 
+import anyio
 import libtorrent as lt
 
 from tvaf import caches
@@ -30,32 +31,30 @@ async def get_torrent_info(handle: lt.torrent_handle) -> lt.torrent_info:
         with ltpy.translate_exceptions():
             return await asyncio.to_thread(handle.torrent_file)
 
-    driver = await services.get_alert_driver()
-    with driver.iter_alerts(
-        lt.alert_category.status,
-        lt.metadata_received_alert,
-        lt.torrent_removed_alert,
-        handle=handle,
-    ) as iterator:
+    async with anyio.create_task_group() as task_group:
+        driver = await services.get_alert_driver()
+        with driver.iter_alerts(
+            lt.alert_category.status,
+            lt.metadata_received_alert,
+            lt.torrent_removed_alert,
+            handle=handle,
+        ) as iterator:
 
-        async def wait_for_metadata() -> None:
-            async for alert in iterator:
-                if isinstance(alert, lt.metadata_received_alert):
-                    break
-                elif isinstance(alert, lt.torrent_removed_alert):
-                    raise ltpy.InvalidTorrentHandleError.create()
+            async def wait_for_metadata() -> None:
+                async for alert in iterator:
+                    if isinstance(alert, lt.metadata_received_alert):
+                        break
+                    elif isinstance(alert, lt.torrent_removed_alert):
+                        raise ltpy.InvalidTorrentHandleError.create()
 
-        # Start the waiter immediately so alert processing can proceed
-        waiter = asyncio.create_task(wait_for_metadata())
-        try:
+            task_group.start_soon(wait_for_metadata)
+
             # Check torrent_file() only after the iterator is created, to
             # ensure we see metadata_received_alert
             ti = await get()
             if ti is not None:
+                task_group.cancel_scope.cancel()
                 return ti
-            await waiter
-        finally:
-            waiter.cancel()
     ti = await get()
     assert ti is not None
     return ti
